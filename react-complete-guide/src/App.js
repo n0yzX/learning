1d34d924c4fa869a1e9fd3880c09e0b8be2afbf7
n0yz <fischer.k92@gmail.com>
2022-10-31 17:33:41 +0100

    311020221733

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,6 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const  App = ()  =>{
   const expenses = [
     { id: 'e1', title: 'Car Insurance', amount: 296.67, date: new Date(2022, 11, 1)},
     { id: 'e2', title: 'Car Insurance', amount: 296.67, date: new Date(2022, 11, 1)},
